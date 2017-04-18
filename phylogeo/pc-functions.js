8ed6f4995439fbc29e691a42847948ba1f8aff73
rbeiko <rbeiko@gmail.com>
2017-04-18 14:31:02 -0300

    Add files via upload

---
+++ phylogeo/pc-functions.js
@@ -0,0 +1,7 @@
+<!--
+function loadPredefinedTree() {
+	var defaultTree = '((A:0.1,(B:0.2,((C:0.3,D:0.4):0.3,E:0.5):0.1):0.1),F:0.2);'
+	const tree = Phylocanvas.createTree('phylocanvas'); // see API for config options
+    tree.load(defaultTree);
+}
+-->
